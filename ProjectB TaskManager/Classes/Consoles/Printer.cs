4f5223e0cadc8a727af2cb8870345333b0e8391f
Marivatis <cyvax2@gmail.com>
2023-12-06 00:07:07 +0200

    Update main menu

--- ProjectB TaskManager/Classes/Consoles/Printer.cs
+++ ProjectB TaskManager/Classes/Consoles/Printer.cs
@@ -13,11 +13,12 @@ namespace ProjectB_TaskManager.Classes.Consoles
             Console.WriteLine("[1] - Add task");
             Console.WriteLine("[2] - Print tasks");
             Console.WriteLine("[3] - Sort tasks by remaining date");
-            Console.WriteLine("[4] - Remove task");
-            Console.WriteLine("[5] - Clear task manager list");
-            Console.WriteLine("[6] - Save tasks to file");
-            Console.WriteLine("[7] - Load tasks from file");
-            Console.WriteLine("[8] - Show main menu");
+            Console.WriteLine("[4] - Mark task as comleted");
+            Console.WriteLine("[5] - Remove task");
+            Console.WriteLine("[6] - Clear task manager list");
+            Console.WriteLine("[7] - Save tasks to file");
+            Console.WriteLine("[8] - Load tasks from file");
+            Console.WriteLine("[9] - Show main menu");
             Console.WriteLine("[0] - Exit");
         }
 
