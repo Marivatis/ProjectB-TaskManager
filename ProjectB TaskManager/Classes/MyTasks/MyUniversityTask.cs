68a9536c1f50a91fa32897daeb2d541492d8317f
Marivatis <cyvax2@gmail.com>
2023-12-05 13:39:12 +0200

    Add MyUniversityTask class

---
+++ ProjectB TaskManager/Classes/MyTasks/MyUniversityTask.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectB_TaskManager.Classes.MyTasks
+{
+    public class MyUniversityTask
+    {
+    }
+}
