9e046285c9ea3e74f342c294c54a1b93ff76ed5a
Marivatis <cyvax2@gmail.com>
2023-12-05 18:12:47 +0200

    Add MyConsole readers

---
+++ ProjectB TaskManager/Classes/Consoles/MyConsoleReader.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace ProjectB_TaskManager.Classes.Consoles
+{
+    internal class MyConsoleReader : Reader
+    {
+        public static int ReadInt32(string inputMessage, int minRange, int maxRange)
+        {
+            int number;
+
+            try
+            {
+                Console.Write(inputMessage);
+                number = ReadInt32(minRange, maxRange);
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Please, select only available options!");
+                number = ReadInt32(inputMessage, minRange, maxRange);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Please, input only integer number!");
+                number = ReadInt32(inputMessage, minRange, maxRange);
+            }
+
+            return number;
+        }
+
+        public static int ReadInt32(string inputMessage)
+        {
+            int number;
+
+            try
+            {
+                Console.Write(inputMessage);
+                number = ReadInt32();
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Please, select only available options!");
+                number = ReadInt32(inputMessage);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Please, input only integer number!");
+                number = ReadInt32(inputMessage);
+            }
+
+            return number;
+        }
+    }
+}
---
+++ ProjectB TaskManager/Classes/Consoles/Reader.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ProjectB_TaskManager.Classes.Consoles
+{
+    internal class Reader
+    {
+        public static int ReadInt32()
+        {
+            return Convert.ToInt32(Console.ReadLine());
+        }
+
+        public static int ReadInt32(int minRange, int maxRange)
+        {
+            int number = ReadInt32();
+
+            if (minRange > number || number > maxRange)
+                throw new OverflowException("Please, input number from the specified range!");
+
+            return number;
+        }
+    }
+}
