cb995cdd7084464ab5c8dbfc5899ad85555302e6
Marivatis <cyvax2@gmail.com>
2023-12-05 17:44:51 +0200

    Add MyConsole class

---
+++ ProjectB TaskManager/Classes/Consoles/MyConsole.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectB_TaskManager.Classes.Consoles
+{
+    public class MyConsole
+    {
+        public void Run()
+        {
+            while (true)
+            {
+
+            }
+        }
+    }
+}
