e127704da52ae4bab4220620a45d5a6ae1bc414a
Marivatis <cyvax2@gmail.com>
2023-12-05 20:50:07 +0200

    Add MyTaskRandomProperties class

---
+++ ProjectB TaskManager/Classes/MyTasks/MyTaskRandomProperties.cs
@@ -0,0 +1,43 @@
+﻿using ProjectB_TaskManager.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectB_TaskManager.Classes.MyTasks
+{
+    public class MyTaskRandomProperties
+    {
+        private Random random;
+
+        public MyTaskRandomProperties() 
+        {
+            random = new Random();
+        }
+
+        public string GetCourseName()
+        {
+            return $"Course {random.Next(1, 8)}";
+        }
+
+        public string GetDescription()
+        {
+            return $"Some Description {random.Next(10, 100)}";
+        }
+
+        public MyTaskStatus GetTaskStatus()
+        {
+            return (MyTaskStatus) random.Next(0, 4);
+        }
+
+        public DateTime GetDeadline()
+        {
+            int year = random.Next(2024, 2035);
+            int manth = random.Next(1, 13);
+            int day = random.Next(1, DateTime.DaysInMonth(year, manth) + 1);
+
+            return new DateTime(year, manth, day);
+        }
+    }
+}
