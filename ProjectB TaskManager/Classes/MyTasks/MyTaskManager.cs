b3e1d1bc912571bd43e1dc0c2be40b5d2ecf1295
Marivatis <cyvax2@gmail.com>
2023-12-05 17:02:25 +0200

    Update methods

--- ProjectB TaskManager/Classes/MyTasks/MyTaskManager.cs
+++ ProjectB TaskManager/Classes/MyTasks/MyTaskManager.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 
 namespace ProjectB_TaskManager.Classes.MyTasks
 {
@@ -25,21 +26,48 @@ namespace ProjectB_TaskManager.Classes.MyTasks
             set { tasks[index] = value; }
         }
 
+        /// <summary>
+        /// Adds task to task manager list.
+        /// </summary>
+        /// <param name="item"></param>
+        /// <exception cref="DuplicateNameException"></exception>
         public void Add(MyTask item)
         {
-            
+            if (IsDuplicate(item))
+            {
+                throw new DuplicateNameException("Task whith this course name and description already exists!");
+            }
+
+            tasks.Add(item);
         }
+        /// <summary>
+        /// Removes all tasks in task manager list.
+        /// </summary>
         public void Clear()
         {
-            throw new NotImplementedException();
+            tasks.Clear();
         }
+        /// <summary>
+        /// Сhecks whether such a task exists in task manager list.
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns>
+        /// Returns <see langword="true"/> if this task exists, otherwise <see langword="false"/>.
+        /// </returns>
         public bool Contains(MyTask item)
         {
-            throw new NotImplementedException();
+            return tasks.Contains(item);
         }
+        /// <summary>
+        /// Removes the first occurrence of a transmitted task in task manager list.
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns>
+        /// Returns <see langword="true"/> if transmitted task has been removed, otherwise <see langword="false"/>.
+        /// </returns>
         public bool Remove(MyTask item)
         {
-            throw new NotImplementedException();
+            return tasks.Remove(item);
         }
         public List<MyTask> ToList()
         {
