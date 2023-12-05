29ff6d2cdebc6fa61493ce8667c3ec5063f32c17
Marivatis <cyvax2@gmail.com>
2023-12-05 18:27:59 +0200

    Add Printer class

---
+++ ProjectB TaskManager/Classes/Consoles/Printer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectB_TaskManager.Classes.Consoles
+{
+    public static class Printer
+    {
+        public static void PrintMainMenu()
+        {
+            Console.WriteLine("[1] - Add task");
+            Console.WriteLine("[2] - Print tasks");
+            Console.WriteLine("[3] - Find tasks");
+            Console.WriteLine("[4] - Delete task");
+            Console.WriteLine("[5] - Clear task");
+            Console.WriteLine("[6] - Save tasks to file");
+            Console.WriteLine("[7] - Load tasks from file");
+            Console.WriteLine("[8] - Show main menu");
+            Console.WriteLine("[0] - Exit");
+        }
+    }
+}
