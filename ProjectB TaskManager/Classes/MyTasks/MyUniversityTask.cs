8bf85cc206e21678c57e6ccb0e18d7a4aad79101
Marivatis <cyvax2@gmail.com>
2023-12-05 13:59:14 +0200

    Update MyUniversityTask inheritance from MyTask

--- ProjectB TaskManager/Classes/MyTasks/MyUniversityTask.cs
+++ ProjectB TaskManager/Classes/MyTasks/MyUniversityTask.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProjectB_TaskManager.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,53 @@ using System.Threading.Tasks;
 
 namespace ProjectB_TaskManager.Classes.MyTasks
 {
-    public class MyUniversityTask
+    public class MyUniversityTask : MyTask
     {
+        public override string Description 
+        {
+            get 
+            {
+                return description; 
+            }
+            set 
+            {
+                if (value.Length > 100)
+                    throw new ArgumentException(nameof(value), "The string lenght must be from 1 to 100 characters!");
+
+                description = value;
+            }
+        }
+        public override MyTaskStatus Status 
+        {
+            get
+            {
+                return status;
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+        public override DateTime Deadline { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public override int GetHashCode()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GetTableFooter()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GetTableHeader()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GetTableRow()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
